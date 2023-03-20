31403b597592dce7bf45c7dab6f8c9ccccb6cbf0
Piyush Makwana <mpiyush3510@gmail.com>
2023-03-20 07:01:37 +0530

    new One add

---
+++ Admin/Admin/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBoxDiv = New System.Windows.Forms.ComboBox()
+        Me.DateTimePickerBdate = New System.Windows.Forms.DateTimePicker()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxRno = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ButtonDelete = New System.Windows.Forms.Button()
+        Me.ButtonRefresh = New System.Windows.Forms.Button()
+        Me.ButtonUpdate = New System.Windows.Forms.Button()
+        Me.ButtonClear = New System.Windows.Forms.Button()
+        Me.ButtonSave = New System.Windows.Forms.Button()
+        Me.DataGridViewText = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridViewText, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.ComboBoxDiv)
+        Me.GroupBox1.Controls.Add(Me.DateTimePickerBdate)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBoxName)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBoxRno)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ButtonDelete)
+        Me.GroupBox1.Controls.Add(Me.ButtonRefresh)
+        Me.GroupBox1.Controls.Add(Me.ButtonUpdate)
+        Me.GroupBox1.Controls.Add(Me.ButtonClear)
+        Me.GroupBox1.Controls.Add(Me.ButtonSave)
+        Me.GroupBox1.Location = New System.Drawing.Point(48, 44)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1124, 464)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "My Detail"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(511, 124)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(88, 25)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Division"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(511, 193)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(101, 25)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "BirthDate"
+        '
+        'ComboBoxDiv
+        '
+        Me.ComboBoxDiv.FormattingEnabled = True
+        Me.ComboBoxDiv.Items.AddRange(New Object() {"SYBCA1", "SYBCA2", "SYBCA3", "SYBCA4", "SYBCA5", "SYBCA6", "SYBCA7", "SYBCA8"})
+        Me.ComboBoxDiv.Location = New System.Drawing.Point(636, 116)
+        Me.ComboBoxDiv.Name = "ComboBoxDiv"
+        Me.ComboBoxDiv.Size = New System.Drawing.Size(191, 33)
+        Me.ComboBoxDiv.TabIndex = 12
+        '
+        'DateTimePickerBdate
+        '
+        Me.DateTimePickerBdate.Location = New System.Drawing.Point(636, 187)
+        Me.DateTimePickerBdate.Name = "DateTimePickerBdate"
+        Me.DateTimePickerBdate.Size = New System.Drawing.Size(403, 31)
+        Me.DateTimePickerBdate.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Verdana", 16.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Coral
+        Me.Label3.Location = New System.Drawing.Point(468, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(160, 52)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Admin"
+        '
+        'TextBoxName
+        '
+        Me.TextBoxName.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.TextBoxName.Location = New System.Drawing.Point(292, 187)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(152, 31)
+        Me.TextBoxName.TabIndex = 9
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(207, 187)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 25)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Name"
+        '
+        'TextBoxRno
+        '
+        Me.TextBoxRno.Location = New System.Drawing.Point(292, 116)
+        Me.TextBoxRno.Name = "TextBoxRno"
+        Me.TextBoxRno.Size = New System.Drawing.Size(152, 31)
+        Me.TextBoxRno.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(207, 116)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 25)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Rno"
+        '
+        'ButtonDelete
+        '
+        Me.ButtonDelete.Location = New System.Drawing.Point(655, 346)
+        Me.ButtonDelete.Name = "ButtonDelete"
+        Me.ButtonDelete.Size = New System.Drawing.Size(115, 45)
+        Me.ButtonDelete.TabIndex = 5
+        Me.ButtonDelete.Text = "Delete"
+        Me.ButtonDelete.UseVisualStyleBackColor = True
+        '
+        'ButtonRefresh
+        '
+        Me.ButtonRefresh.Location = New System.Drawing.Point(864, 346)
+        Me.ButtonRefresh.Name = "ButtonRefresh"
+        Me.ButtonRefresh.Size = New System.Drawing.Size(115, 45)
+        Me.ButtonRefresh.TabIndex = 4
+        Me.ButtonRefresh.Text = "Refresh"
+        Me.ButtonRefresh.UseVisualStyleBackColor = True
+        '
+        'ButtonUpdate
+        '
+        Me.ButtonUpdate.Location = New System.Drawing.Point(250, 346)
+        Me.ButtonUpdate.Name = "ButtonUpdate"
+        Me.ButtonUpdate.Size = New System.Drawing.Size(115, 45)
+        Me.ButtonUpdate.TabIndex = 3
+        Me.ButtonUpdate.Text = "Update"
+        Me.ButtonUpdate.UseVisualStyleBackColor = True
+        '
+        'ButtonClear
+        '
+        Me.ButtonClear.Location = New System.Drawing.Point(459, 346)
+        Me.ButtonClear.Name = "ButtonClear"
+        Me.ButtonClear.Size = New System.Drawing.Size(115, 45)
+        Me.ButtonClear.TabIndex = 2
+        Me.ButtonClear.Text = "Clear"
+        Me.ButtonClear.UseVisualStyleBackColor = True
+        '
+        'ButtonSave
+        '
+        Me.ButtonSave.Location = New System.Drawing.Point(60, 346)
+        Me.ButtonSave.Name = "ButtonSave"
+        Me.ButtonSave.Size = New System.Drawing.Size(115, 45)
+        Me.ButtonSave.TabIndex = 0
+        Me.ButtonSave.Text = "Save"
+        Me.ButtonSave.UseVisualStyleBackColor = True
+        '
+        'DataGridViewText
+        '
+        Me.DataGridViewText.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridViewText.Location = New System.Drawing.Point(48, 514)
+        Me.DataGridViewText.Name = "DataGridViewText"
+        Me.DataGridViewText.RowTemplate.Height = 33
+        Me.DataGridViewText.Size = New System.Drawing.Size(1124, 218)
+        Me.DataGridViewText.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1227, 787)
+        Me.Controls.Add(Me.DataGridViewText)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DataGridViewText, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ComboBoxDiv As System.Windows.Forms.ComboBox
+    Friend WithEvents DateTimePickerBdate As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxName As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxRno As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ButtonDelete As System.Windows.Forms.Button
+    Friend WithEvents ButtonRefresh As System.Windows.Forms.Button
+    Friend WithEvents ButtonUpdate As System.Windows.Forms.Button
+    Friend WithEvents ButtonClear As System.Windows.Forms.Button
+    Friend WithEvents ButtonSave As System.Windows.Forms.Button
+    Friend WithEvents DataGridViewText As System.Windows.Forms.DataGridView
+
+End Class
---
+++ Admin/Admin/Form1.vb
@@ -0,0 +1,164 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim dataString As String = "Data Source=.\SQLEXPRESS;AttachDbFilename=D:\Visual Basic\vs_2010\Admin\Admin\Admin.mdf;Integrated Security=True;User Instance=True"
+    Dim con As SqlConnection
+    Dim cmd As SqlCommand
+    Dim ad As SqlDataAdapter
+    Dim dt As DataSet
+
+    Private Sub connection()
+        con = New SqlConnection(dataString)
+        con.Open()
+        Try
+            If con.State = ConnectionState.Open Then
+                'MsgBox("You are Connected")
+            End If
+        Catch ec As Exception
+            MsgBox(ec.Message)
+        End Try
+    End Sub
+
+    Private Sub createTable()
+        cmd = New SqlCommand("create table Admin(Rno int primary key ,Name varchar(30),Div varchar(10),Bdate date)", con)
+        Try
+            cmd.ExecuteNonQuery()
+            MsgBox("create table")
+        Catch ec As Exception
+            'MsgBox(ec.Message)
+        End Try
+
+    End Sub
+    Private Sub insertData()
+        validation()
+        cmd = New SqlCommand("insert into Admin values(@Rno,@Name,@Div,@Bdate)", con)
+        Try
+            cmd.Parameters.AddWithValue("@Rno", Convert.ToInt32(TextBoxRno.Text))
+            cmd.Parameters.AddWithValue("@Name", TextBoxName.Text)
+            cmd.Parameters.AddWithValue("@Div", ComboBoxDiv.Text)
+            cmd.Parameters.AddWithValue("@Bdate", DateTimePickerBdate.Value.ToShortDateString)
+            cmd.ExecuteNonQuery()
+            MsgBox("Inserted")
+            clearData()
+            GetData()
+        Catch ex As Exception
+            'MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub GetData()
+        ad = New SqlDataAdapter("Select * from Admin", con)
+        dt = New DataSet
+        ad.Fill(dt, "Admin")
+        Try
+            DataGridViewText.DataSource = dt.Tables(0)
+        Catch
+            MsgBox("Error to Fetch data")
+        End Try
+    End Sub
+
+    Private Sub deleteData()
+        If DataGridViewText.RowCount = 0 Then
+            MsgBox("Empty data")
+        End If
+
+        If MsgBox("Want to deleted data ?", MsgBoxStyle.Question + MsgBoxStyle.OkCancel, "Confirmation") = MsgBoxResult.Cancel Then Return
+
+        If DataGridViewText.AreAllCellsSelected(0) = True Then
+            cmd = New SqlCommand("Delete from Admin", con)
+            Try
+                cmd.ExecuteNonQuery()
+                MsgBox("Deleted All")
+                GetData()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+
+        For Each row As DataGridViewRow In DataGridViewText.SelectedRows
+            If row.Selected Then
+                Try
+                    cmd = New SqlCommand("delete from Admin where rno='" & row.DataBoundItem(0).ToString & "'", con)
+                    cmd.ExecuteNonQuery()
+                    MsgBox("Deleted")
+                    GetData()
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+            End If
+        Next
+    End Sub
+
+    Private Sub updateData()
+        If MsgBox("Udate data roll number wise !", MsgBoxStyle.OkOnly, "Admin Info") = MsgBoxResult.Ok Then Return
+
+        cmd = New SqlCommand("update Admin set name=@Name,div=@Div,bdate=@Bdate where rno=@Rno", con)
+        Try
+            cmd.Parameters.AddWithValue("@Rno", Convert.ToInt32(TextBoxRno.Text))
+            cmd.Parameters.AddWithValue("@Name", TextBoxName.Text)
+            cmd.Parameters.AddWithValue("@Div", ComboBoxDiv.Text)
+            cmd.Parameters.AddWithValue("@Bdate", DateTimePickerBdate.Value.ToShortDateString)
+            cmd.ExecuteNonQuery()
+            MsgBox("Update")
+            clearData()
+            GetData()
+        Catch ex As Exception
+            'MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub validation()
+        If TextBoxRno.Text = "" And TextBoxName.Text = "" And ComboBoxDiv.Text = "" Then
+            MsgBox("All Values Are Empty")
+            Return
+        End If
+
+        If TextBoxRno.Text = "" Then
+            MsgBox("Please Enter Rno")
+            Return
+        End If
+
+        If TextBoxName.Text = "" Then
+            MsgBox("Please Enter Name")
+            Return
+        End If
+
+        If ComboBoxDiv.Text = "" Then
+            MsgBox("Choose Correct Option")
+            Return
+        End If
+    End Sub
+
+    Private Sub clearData()
+        TextBoxRno.Clear()
+        TextBoxName.Clear()
+        ComboBoxDiv.Text = ""
+        DateTimePickerBdate.ResetText()
+    End Sub
+
+    Private Sub ButtonSave_Click(sender As System.Object, e As System.EventArgs) Handles ButtonSave.Click
+        connection()
+        createTable()
+        insertData()
+    End Sub
+
+    Private Sub ButtonRefresh_Click(sender As System.Object, e As System.EventArgs) Handles ButtonRefresh.Click
+        connection()
+        GetData()
+    End Sub
+
+    Private Sub ButtonDelete_Click(sender As System.Object, e As System.EventArgs) Handles ButtonDelete.Click
+        connection()
+        deleteData()
+    End Sub
+
+    Private Sub ButtonClear_Click(sender As System.Object, e As System.EventArgs) Handles ButtonClear.Click
+        clearData()
+    End Sub
+
+    Private Sub ButtonUpdate_Click(sender As System.Object, e As System.EventArgs) Handles ButtonUpdate.Click
+        connection()
+        updateData()
+    End Sub
+End Class
